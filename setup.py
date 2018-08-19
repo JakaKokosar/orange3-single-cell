ef9a709cb9c487422cd7e972badf1720c78bbfd4
JakaKokosar <jaka.kokosar@gmail.com>
2018-08-19 21:34:01 +0200

    appveyor: add bioconda channel

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ LICENSE = 'GPLv3+'
 KEYWORDS = (
     # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
     # can be installed using the Orange Add-on Manager
-    'orange3 add-on',
+    # 'orange3 add-on',
 )
 
 PACKAGES = find_packages()
